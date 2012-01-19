7614a9200e15dd00e9205bb01a3adcdcd34c85e8
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-01-19 11:44:13 +1100

    Changes the depencies to Clojure 1.3.0 and Midje to 1.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clj-predicates "0.2.0-SNAPSHOT"
   :description "A small library of useful predicate functions missing in the core Clojure library"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [midje "1.2-beta1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [midje "1.3.1"]])
